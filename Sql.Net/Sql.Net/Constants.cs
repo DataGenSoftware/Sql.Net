25ced02a43419e427956a8d8eb3873e77d648778
karolhabczyk <karol.habczyk@gmail.com>
2014-02-20 22:35:56 +0100

    Added Constants class

---
+++ Sql.Net/Sql.Net/Constants.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+
+namespace Sql.Net
+{
+    public static class Constants
+    {
+        #region Math
+
+        [SqlFunction()]
+        public static Double Pi()
+        {
+            return Math.PI;
+        }
+
+        [SqlFunction()]
+        public static Double E()
+        {
+            return Math.E;
+        }
+
+        #endregion
+    }
+}
