d407fc2b4c3dba2ce94259bac04edc971b42fd0b
karolhabczyk <karol.habczyk@gmail.com>
2015-03-27 20:55:32 +0100

    #23 Shorten function names

--- Sql.Net/Sql scripts/Dev/Sql.Net.uninstall.sql
+++ Sql.Net/Sql scripts/Dev/Sql.Net.uninstall.sql
@@ -32,150 +32,150 @@ GO
 DROP SYNONYM [dbo].[mode]
 GO
 
-DROP FUNCTION [Sql.Net].[Math.Const.Pi]
+DROP FUNCTION [Sql.Net].[Math.Pi]
 GO
-DROP FUNCTION [Sql.Net].[Math.Const.E]
-GO
-
-DROP FUNCTION [Sql.Net].[Types.Bool.ToString]
-GO
-DROP FUNCTION [Sql.Net].[Types.Bool.FalseString]
-GO
-DROP FUNCTION [Sql.Net].[Types.Bool.TrueString]
-GO
-DROP FUNCTION [Sql.Net].[Types.Bool.TryParse]
+DROP FUNCTION [Sql.Net].[Math.E]
 GO
 
-DROP FUNCTION [Sql.Net].[Types.Char.IsControl]
+DROP FUNCTION [Sql.Net].[Bool.ToString]
 GO
-DROP FUNCTION [Sql.Net].[Types.Char.IsDigit]
+DROP FUNCTION [Sql.Net].[Bool.FalseString]
 GO
-DROP FUNCTION [Sql.Net].[Types.Char.IsLetter]
+DROP FUNCTION [Sql.Net].[Bool.TrueString]
 GO
-DROP FUNCTION [Sql.Net].[Types.Char.IsLetterOrDigit]
-GO
-DROP FUNCTION [Sql.Net].[Types.Char.IsLower]
-GO
-DROP FUNCTION [Sql.Net].[Types.Char.IsNumber]
-GO
-DROP FUNCTION [Sql.Net].[Types.Char.IsPunctuation]
-GO
-DROP FUNCTION [Sql.Net].[Types.Char.IsSeparator]
-GO
-DROP FUNCTION [Sql.Net].[Types.Char.IsSymbol]
-GO
-DROP FUNCTION [Sql.Net].[Types.Char.IsUpper]
-GO
-DROP FUNCTION [Sql.Net].[Types.Char.IsWhiteSpace]
+DROP FUNCTION [Sql.Net].[Bool.TryParse]
 GO
 
-DROP FUNCTION [Sql.Net].[Types.DateTime.ToString]
+DROP FUNCTION [Sql.Net].[Char.IsControl]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.DaysInMonth]
+DROP FUNCTION [Sql.Net].[Char.IsDigit]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.DaysInYear]
+DROP FUNCTION [Sql.Net].[Char.IsLetter]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.IsLeapYear]
+DROP FUNCTION [Sql.Net].[Char.IsLetterOrDigit]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.Date]
+DROP FUNCTION [Sql.Net].[Char.IsLower]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.TimeOfDay]
+DROP FUNCTION [Sql.Net].[Char.IsNumber]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.IsWeekendDay]
+DROP FUNCTION [Sql.Net].[Char.IsPunctuation]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.IsWeekDay]
+DROP FUNCTION [Sql.Net].[Char.IsSeparator]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.IsToday]
+DROP FUNCTION [Sql.Net].[Char.IsSymbol]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.BeginingOfDay]
+DROP FUNCTION [Sql.Net].[Char.IsUpper]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.EndOfDay]
+DROP FUNCTION [Sql.Net].[Char.IsWhiteSpace]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.BeginingOfMonth]
+
+DROP FUNCTION [Sql.Net].[DateTime.ToString]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.EndOfMonth]
+DROP FUNCTION [Sql.Net].[DateTime.DaysInMonth]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.FirstDayOfMonth]
+DROP FUNCTION [Sql.Net].[DateTime.DaysInYear]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.LastDayOfMonth]
+DROP FUNCTION [Sql.Net].[DateTime.IsLeapYear]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.BeginingOfWeek]
+DROP FUNCTION [Sql.Net].[DateTime.Date]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.EndOfWeek]
+DROP FUNCTION [Sql.Net].[DateTime.TimeOfDay]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.BeginingOfYear]
+DROP FUNCTION [Sql.Net].[DateTime.IsWeekendDay]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.EndOfYear]
+DROP FUNCTION [Sql.Net].[DateTime.IsWeekDay]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.IsHoliday]
+DROP FUNCTION [Sql.Net].[DateTime.IsToday]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.DaysBetweenDates]
+DROP FUNCTION [Sql.Net].[DateTime.BeginingOfDay]
 GO
-DROP FUNCTION [Sql.Net].[Types.DateTime.QuarterNumberOfyear]
+DROP FUNCTION [Sql.Net].[DateTime.EndOfDay]
+GO
+DROP FUNCTION [Sql.Net].[DateTime.BeginingOfMonth]
+GO
+DROP FUNCTION [Sql.Net].[DateTime.EndOfMonth]
+GO
+DROP FUNCTION [Sql.Net].[DateTime.FirstDayOfMonth]
+GO
+DROP FUNCTION [Sql.Net].[DateTime.LastDayOfMonth]
+GO
+DROP FUNCTION [Sql.Net].[DateTime.BeginingOfWeek]
+GO
+DROP FUNCTION [Sql.Net].[DateTime.EndOfWeek]
+GO
+DROP FUNCTION [Sql.Net].[DateTime.BeginingOfYear]
+GO
+DROP FUNCTION [Sql.Net].[DateTime.EndOfYear]
+GO
+DROP FUNCTION [Sql.Net].[DateTime.IsHoliday]
+GO
+DROP FUNCTION [Sql.Net].[DateTime.DaysBetweenDates]
+GO
+DROP FUNCTION [Sql.Net].[DateTime.QuarterNumberOfyear]
 GO
 DROP SYNONYM [dbo].[quarter]
 GO
 
-DROP FUNCTION [Sql.Net].[Types.Decimal.ToString]
+DROP FUNCTION [Sql.Net].[Decimal.ToString]
 GO
-DROP FUNCTION [Sql.Net].[Types.Decimal.TryParse]
-GO
-
-DROP FUNCTION [Sql.Net].[Types.Double.ToString]
-GO
-DROP FUNCTION [Sql.Net].[Types.Double.TryParse]
+DROP FUNCTION [Sql.Net].[Decimal.TryParse]
 GO
 
-DROP FUNCTION [Sql.Net].[Types.Int.ToString]
+DROP FUNCTION [Sql.Net].[Double.ToString]
 GO
-DROP FUNCTION [Sql.Net].[Types.Int.TryParse]
-GO
-DROP FUNCTION [Sql.Net].[Types.Int.MinValue]
-GO
-DROP FUNCTION [Sql.Net].[Types.Int.MaxValue]
-GO
-DROP FUNCTION [Sql.Net].[Types.Int.ToWordsPL]
+DROP FUNCTION [Sql.Net].[Double.TryParse]
 GO
 
-DROP FUNCTION [Sql.Net].[Types.Single.ToString]
+DROP FUNCTION [Sql.Net].[Int.ToString]
 GO
-DROP FUNCTION [Sql.Net].[Types.Single.TryParse]
+DROP FUNCTION [Sql.Net].[Int.TryParse]
 GO
-
-DROP FUNCTION [Sql.Net].[Types.String.Contains]
+DROP FUNCTION [Sql.Net].[Int.MinValue]
 GO
-DROP FUNCTION [Sql.Net].[Types.String.EndsWith]
+DROP FUNCTION [Sql.Net].[Int.MaxValue]
 GO
-DROP FUNCTION [Sql.Net].[Types.String.StartsWith]
-GO
-DROP FUNCTION [Sql.Net].[Types.String.IndexOf]
-GO
-DROP FUNCTION [Sql.Net].[Types.String.LastIndexOf]
-GO
-DROP FUNCTION [Sql.Net].[Types.String.Insert]
-GO
-DROP FUNCTION [Sql.Net].[Types.String.PadLeft]
-GO
-DROP FUNCTION [Sql.Net].[Types.String.PadRight]
-GO
-DROP FUNCTION [Sql.Net].[Types.String.Split]
-GO
-DROP FUNCTION [Sql.Net].[Types.String.TextGetBetweenTexts]
-GO
-DROP FUNCTION [Sql.Net].[Types.String.TripleDESEncrypt]
-GO
-DROP FUNCTION [Sql.Net].[Types.String.TripleDESDecrypt]
-GO
-DROP FUNCTION [Sql.Net].[Types.String.MD5ComputeHash]
-GO
-DROP FUNCTION [Sql.Net].[Types.String.MD5VerifyHash]
-GO
-DROP FUNCTION [Sql.Net].[Types.String.SHA1ComputeHash]
-GO
-DROP FUNCTION [Sql.Net].[Types.String.SHA1VerifyHash]
+DROP FUNCTION [Sql.Net].[Int.ToWordsPL]
 GO
 
-DROP FUNCTION [Sql.Net].[System.EnvironmentVariables.Get]
+DROP FUNCTION [Sql.Net].[Single.ToString]
+GO
+DROP FUNCTION [Sql.Net].[Single.TryParse]
+GO
+
+DROP FUNCTION [Sql.Net].[String.Contains]
+GO
+DROP FUNCTION [Sql.Net].[String.EndsWith]
+GO
+DROP FUNCTION [Sql.Net].[String.StartsWith]
+GO
+DROP FUNCTION [Sql.Net].[String.IndexOf]
+GO
+DROP FUNCTION [Sql.Net].[String.LastIndexOf]
+GO
+DROP FUNCTION [Sql.Net].[String.Insert]
+GO
+DROP FUNCTION [Sql.Net].[String.PadLeft]
+GO
+DROP FUNCTION [Sql.Net].[String.PadRight]
+GO
+DROP FUNCTION [Sql.Net].[String.Split]
+GO
+DROP FUNCTION [Sql.Net].[String.TextGetBetweenTexts]
+GO
+DROP FUNCTION [Sql.Net].[String.TripleDESEncrypt]
+GO
+DROP FUNCTION [Sql.Net].[String.TripleDESDecrypt]
+GO
+DROP FUNCTION [Sql.Net].[String.MD5ComputeHash]
+GO
+DROP FUNCTION [Sql.Net].[String.MD5VerifyHash]
+GO
+DROP FUNCTION [Sql.Net].[String.SHA1ComputeHash]
+GO
+DROP FUNCTION [Sql.Net].[String.SHA1VerifyHash]
+GO
+
+DROP FUNCTION [Sql.Net].[System.EnvironmentVariableGet]
 GO
 
 DROP SCHEMA [Sql.Net]
--- Sql.Net/Sql scripts/Prod/Sql.Net.install.sql
+++ Sql.Net/Sql scripts/Prod/Sql.Net.install.sql
@@ -75,13 +75,13 @@ END
 GO
 CREATE PROCEDURE [Sql.Net].[Configuration.HolidayAdd] @date datetime AS
 BEGIN
-	SET @date = [Sql.Net].[Types.DateTime.Date](@date)
+	SET @date = [Sql.Net].[DateTime.Date](@date)
 	INSERT INTO [Sql.Net].[Configuration.Holidays] ([Date]) VALUES (@date)
 END
 GO
 CREATE PROCEDURE [Sql.Net].[Configuration.HolidayRemove] @date datetime AS
 BEGIN
-	SET @date = [Sql.Net].[Types.DateTime.Date](@date)
+	SET @date = [Sql.Net].[DateTime.Date](@date)
 	DELETE FROM [Sql.Net].[Configuration.Holidays] WHERE [Date] = @date
 END
 GO
@@ -102,127 +102,127 @@ GO
 CREATE SYNONYM [dbo].[mode] FOR [Sql.Net].[Aggregate.Mode]
 GO
 
-CREATE FUNCTION [Sql.Net].[Math.Const.Pi]() RETURNS float
+CREATE FUNCTION [Sql.Net].[Math.Pi]() RETURNS float
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Mathematic.Constants].[Pi]
 GO
-CREATE FUNCTION [Sql.Net].[Math.Const.E]() RETURNS float
+CREATE FUNCTION [Sql.Net].[Math.E]() RETURNS float
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Mathematic.Constants].[E]
 GO
 
-CREATE FUNCTION [Sql.Net].[Types.Bool.ToString](@value bit) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[Bool.ToString](@value bit) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.BooleanType].[ToString]
 GO
-CREATE FUNCTION [Sql.Net].[Types.Bool.FalseString]() RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[Bool.FalseString]() RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.BooleanType].[FalseString]
 GO
-CREATE FUNCTION [Sql.Net].[Types.Bool.TrueString]() RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[Bool.TrueString]() RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.BooleanType].[TrueString]
 GO
-CREATE FUNCTION [Sql.Net].[Types.Bool.TryParse](@value nvarchar(max)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[Bool.TryParse](@value nvarchar(max)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.BooleanType].[TryParse]
 GO
 
-CREATE FUNCTION [Sql.Net].[Types.Char.IsControl](@character nchar(1)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[Char.IsControl](@character nchar(1)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.CharType].[IsControl]
 GO
-CREATE FUNCTION [Sql.Net].[Types.Char.IsDigit](@character nchar(1)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[Char.IsDigit](@character nchar(1)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.CharType].[IsDigit]
 GO
-CREATE FUNCTION [Sql.Net].[Types.Char.IsLetter](@character nchar(1)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[Char.IsLetter](@character nchar(1)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.CharType].[IsLetter]
 GO
-CREATE FUNCTION [Sql.Net].[Types.Char.IsLetterOrDigit](@character nchar(1)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[Char.IsLetterOrDigit](@character nchar(1)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.CharType].[IsLetterOrDigit]
 GO
-CREATE FUNCTION [Sql.Net].[Types.Char.IsLower](@character nchar(1)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[Char.IsLower](@character nchar(1)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.CharType].[IsLower]
 GO
-CREATE FUNCTION [Sql.Net].[Types.Char.IsNumber](@character nchar(1)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[Char.IsNumber](@character nchar(1)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.CharType].[IsNumber]
 GO
-CREATE FUNCTION [Sql.Net].[Types.Char.IsPunctuation](@character nchar(1)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[Char.IsPunctuation](@character nchar(1)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.CharType].[IsPunctuation]
 GO
-CREATE FUNCTION [Sql.Net].[Types.Char.IsSeparator](@character nchar(1)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[Char.IsSeparator](@character nchar(1)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.CharType].[IsSeparator]
 GO
-CREATE FUNCTION [Sql.Net].[Types.Char.IsSymbol](@character nchar(1)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[Char.IsSymbol](@character nchar(1)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.CharType].[IsSymbol]
 GO
-CREATE FUNCTION [Sql.Net].[Types.Char.IsUpper](@character nchar(1)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[Char.IsUpper](@character nchar(1)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.CharType].[IsUpper]
 GO
-CREATE FUNCTION [Sql.Net].[Types.Char.IsWhiteSpace](@character nchar(1)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[Char.IsWhiteSpace](@character nchar(1)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.CharType].[IsWhiteSpace]
 GO
 
-CREATE FUNCTION [Sql.Net].[Types.DateTime.ToString](@dateTime datetime, @format nvarchar(max)) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[DateTime.ToString](@dateTime datetime, @format nvarchar(max)) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateTimeToString
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.DaysInMonth](@year int, @month int) RETURNS int
+CREATE FUNCTION [Sql.Net].[DateTime.DaysInMonth](@year int, @month int) RETURNS int
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateTimeDaysInMonth
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.DaysInYear](@year int) RETURNS int
+CREATE FUNCTION [Sql.Net].[DateTime.DaysInYear](@year int) RETURNS int
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateTimeDaysInYear
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.IsLeapYear](@year int) RETURNS bit
+CREATE FUNCTION [Sql.Net].[DateTime.IsLeapYear](@year int) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateTimeIsLeapYear
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.Date](@dateTime datetime) RETURNS datetime
+CREATE FUNCTION [Sql.Net].[DateTime.Date](@dateTime datetime) RETURNS datetime
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateTimeDate
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.TimeOfDay](@dateTime datetime) RETURNS time
+CREATE FUNCTION [Sql.Net].[DateTime.TimeOfDay](@dateTime datetime) RETURNS time
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateTimeTimeOfDay
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.IsWeekendDay](@dateTime datetime) RETURNS bit
+CREATE FUNCTION [Sql.Net].[DateTime.IsWeekendDay](@dateTime datetime) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateTimeIsWeekendDay
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.IsWeekDay](@dateTime datetime) RETURNS bit
+CREATE FUNCTION [Sql.Net].[DateTime.IsWeekDay](@dateTime datetime) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateTimeIsWeekDay
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.IsToday](@dateTime datetime) RETURNS bit
+CREATE FUNCTION [Sql.Net].[DateTime.IsToday](@dateTime datetime) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateTimeIsToday
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.BeginingOfDay](@dateTime datetime) RETURNS datetime
+CREATE FUNCTION [Sql.Net].[DateTime.BeginingOfDay](@dateTime datetime) RETURNS datetime
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateBeginingOfDay
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.EndOfDay](@dateTime datetime) RETURNS datetime
+CREATE FUNCTION [Sql.Net].[DateTime.EndOfDay](@dateTime datetime) RETURNS datetime
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateEndOfDay
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.BeginingOfMonth](@dateTime datetime) RETURNS datetime
+CREATE FUNCTION [Sql.Net].[DateTime.BeginingOfMonth](@dateTime datetime) RETURNS datetime
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateBeginingOfMonth
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.EndOfMonth](@dateTime datetime) RETURNS datetime
+CREATE FUNCTION [Sql.Net].[DateTime.EndOfMonth](@dateTime datetime) RETURNS datetime
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateEndOfMonth
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.FirstDayOfMonth](@dateTime datetime) RETURNS datetime
+CREATE FUNCTION [Sql.Net].[DateTime.FirstDayOfMonth](@dateTime datetime) RETURNS datetime
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateFirstDayOfMonth
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.LastDayOfMonth](@dateTime datetime) RETURNS datetime
+CREATE FUNCTION [Sql.Net].[DateTime.LastDayOfMonth](@dateTime datetime) RETURNS datetime
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateLastDayOfMonth
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.BeginingOfWeek](@dateTime datetime) RETURNS datetime
+CREATE FUNCTION [Sql.Net].[DateTime.BeginingOfWeek](@dateTime datetime) RETURNS datetime
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateBeginingOfWeek
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.EndOfWeek](@dateTime datetime) RETURNS datetime
+CREATE FUNCTION [Sql.Net].[DateTime.EndOfWeek](@dateTime datetime) RETURNS datetime
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateEndOfWeek
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.BeginingOfYear](@dateTime datetime) RETURNS datetime
+CREATE FUNCTION [Sql.Net].[DateTime.BeginingOfYear](@dateTime datetime) RETURNS datetime
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateBeginingOfYear
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.EndOfYear](@dateTime datetime) RETURNS datetime
+CREATE FUNCTION [Sql.Net].[DateTime.EndOfYear](@dateTime datetime) RETURNS datetime
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DateTimeType].DateEndOfYear
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.IsHoliday](@dateTime datetime) RETURNS BIT AS
+CREATE FUNCTION [Sql.Net].[DateTime.IsHoliday](@dateTime datetime) RETURNS BIT AS
 BEGIN
-	SET @dateTime = [Sql.Net].[Types.DateTime.Date](@dateTime)
+	SET @dateTime = [Sql.Net].[DateTime.Date](@dateTime)
 	RETURN 
 		(SELECT CONVERT(BIT, COUNT([Date])) 
 			FROM [Sql.Net].[Configuration.Holidays] 
 			WHERE [Date] =  @dateTime)
 END
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.DaysBetweenDates](@startDate DATETIME, @endDate DATETIME)
+CREATE FUNCTION [Sql.Net].[DateTime.DaysBetweenDates](@startDate DATETIME, @endDate DATETIME)
 RETURNS @dates TABLE
 (
 	[Date] DATETIME
@@ -240,102 +240,102 @@ BEGIN
 
 END
 GO
-CREATE FUNCTION [Sql.Net].[Types.DateTime.QuarterNumberOfyear](@dateTime datetime)
+CREATE FUNCTION [Sql.Net].[DateTime.QuarterNumberOfyear](@dateTime datetime)
 RETURNS INT
 AS
 BEGIN
 	RETURN DATEPART(quarter, @dateTime)
 END
 GO
-CREATE SYNONYM [dbo].[quarter] FOR [Sql.Net].[Types.DateTime.QuarterNumberOfyear]
+CREATE SYNONYM [dbo].[quarter] FOR [Sql.Net].[DateTime.QuarterNumberOfyear]
 GO
 
-CREATE FUNCTION [Sql.Net].[Types.Decimal.ToString](@value decimal(18, 4), @format nvarchar(max)) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[Decimal.ToString](@value decimal(18, 4), @format nvarchar(max)) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DecimalType].DecimalToString
 GO
-CREATE FUNCTION [Sql.Net].[Types.Decimal.TryParse](@value nvarchar(max)) RETURNS decimal(18, 4)
+CREATE FUNCTION [Sql.Net].[Decimal.TryParse](@value nvarchar(max)) RETURNS decimal(18, 4)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DecimalType].DecimalTryParse
 GO
 
-CREATE FUNCTION [Sql.Net].[Types.Double.ToString](@value float, @format nvarchar(max)) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[Double.ToString](@value float, @format nvarchar(max)) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DoubleType].DoubleToString
 GO
-CREATE FUNCTION [Sql.Net].[Types.Double.TryParse](@value nvarchar(max)) RETURNS float
+CREATE FUNCTION [Sql.Net].[Double.TryParse](@value nvarchar(max)) RETURNS float
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.DoubleType].DoubleTryParse
 GO
 
-CREATE FUNCTION [Sql.Net].[Types.Int.ToString](@value int, @format nvarchar(max)) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[Int.ToString](@value int, @format nvarchar(max)) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.IntType].IntToString
 GO
-CREATE FUNCTION [Sql.Net].[Types.Int.TryParse](@value nvarchar(max)) RETURNS int
+CREATE FUNCTION [Sql.Net].[Int.TryParse](@value nvarchar(max)) RETURNS int
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.IntType].IntTryParse
 GO
-CREATE FUNCTION [Sql.Net].[Types.Int.MinValue]() RETURNS int
+CREATE FUNCTION [Sql.Net].[Int.MinValue]() RETURNS int
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.IntType].IntMinValue
 GO
-CREATE FUNCTION [Sql.Net].[Types.Int.MaxValue]() RETURNS int
+CREATE FUNCTION [Sql.Net].[Int.MaxValue]() RETURNS int
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.IntType].IntMaxValue
 GO
-CREATE FUNCTION [Sql.Net].[Types.Int.ToWordsPL](@value int) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[Int.ToWordsPL](@value int) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.IntType].IntToWordsPL
 GO
 
-CREATE FUNCTION [Sql.Net].[Types.Single.ToString](@value real, @format nvarchar(max)) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[Single.ToString](@value real, @format nvarchar(max)) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.SingleType].SingleToString
 GO
-CREATE FUNCTION [Sql.Net].[Types.Single.TryParse](@value nvarchar(max)) RETURNS real
+CREATE FUNCTION [Sql.Net].[Single.TryParse](@value nvarchar(max)) RETURNS real
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.SingleType].SingleTryParse
 GO
 
-CREATE FUNCTION [Sql.Net].[Types.String.Contains](@text nvarchar(max), @value nvarchar(max)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[String.Contains](@text nvarchar(max), @value nvarchar(max)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringContains
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.EndsWith](@text nvarchar(max), @value nvarchar(max)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[String.EndsWith](@text nvarchar(max), @value nvarchar(max)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringEndsWith
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.StartsWith](@text nvarchar(max), @value nvarchar(max)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[String.StartsWith](@text nvarchar(max), @value nvarchar(max)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringStartsWith
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.IndexOf](@text nvarchar(max), @value nvarchar(max)) RETURNS int
+CREATE FUNCTION [Sql.Net].[String.IndexOf](@text nvarchar(max), @value nvarchar(max)) RETURNS int
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringIndexOf
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.LastIndexOf](@text nvarchar(max), @value nvarchar(max)) RETURNS int
+CREATE FUNCTION [Sql.Net].[String.LastIndexOf](@text nvarchar(max), @value nvarchar(max)) RETURNS int
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringLastIndexOf
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.Insert](@text nvarchar(max), @startIndex int, @value nvarchar(max)) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[String.Insert](@text nvarchar(max), @startIndex int, @value nvarchar(max)) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringInsert
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.PadLeft](@text nvarchar(max), @totalWidth int) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[String.PadLeft](@text nvarchar(max), @totalWidth int) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringPadLeft
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.PadRight](@text nvarchar(max), @totalWidth int) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[String.PadRight](@text nvarchar(max), @totalWidth int) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringPadRight
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.Split](@text nvarchar(max), @separator nvarchar(max)) RETURNS table (string nvarchar(max))
+CREATE FUNCTION [Sql.Net].[String.Split](@text nvarchar(max), @separator nvarchar(max)) RETURNS table (string nvarchar(max))
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringSplit
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.TextGetBetweenTexts](@text nvarchar(max), @startText nvarchar(max), @endText nvarchar(max)) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[String.TextGetBetweenTexts](@text nvarchar(max), @startText nvarchar(max), @endText nvarchar(max)) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringTextGetBetweenTexts
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.TripleDESEncrypt](@text nvarchar(max), @key nvarchar(max)) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[String.TripleDESEncrypt](@text nvarchar(max), @key nvarchar(max)) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringTripleDESEncrypt
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.TripleDESDecrypt](@text nvarchar(max), @key nvarchar(max)) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[String.TripleDESDecrypt](@text nvarchar(max), @key nvarchar(max)) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringTripleDESDecrypt
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.MD5ComputeHash](@text nvarchar(max)) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[String.MD5ComputeHash](@text nvarchar(max)) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringMD5ComputeHash
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.MD5VerifyHash](@text nvarchar(max), @hash nvarchar(max)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[String.MD5VerifyHash](@text nvarchar(max), @hash nvarchar(max)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringMD5VerifyHash
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.SHA1ComputeHash](@text nvarchar(max)) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[String.SHA1ComputeHash](@text nvarchar(max)) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringSHA1ComputeHash
 GO
-CREATE FUNCTION [Sql.Net].[Types.String.SHA1VerifyHash](@text nvarchar(max), @hash nvarchar(max)) RETURNS bit
+CREATE FUNCTION [Sql.Net].[String.SHA1VerifyHash](@text nvarchar(max), @hash nvarchar(max)) RETURNS bit
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.Types.StringType].StringSHA1VerifyHash
 GO
 
-CREATE FUNCTION [Sql.Net].[System.EnvironmentVariables.Get](@name nvarchar(max)) RETURNS nvarchar(max)
+CREATE FUNCTION [Sql.Net].[System.EnvironmentVariableGet](@name nvarchar(max)) RETURNS nvarchar(max)
 AS EXTERNAL NAME [Sql.Net].[Sql.Net.System.EnvironmentVariables].Get
 GO
